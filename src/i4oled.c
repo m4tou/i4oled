e5bfac72049861d07446f75abd39ed055aa69848
Przemo Firszt <przemo@firszt.eu>
2012-10-01 22:30:54 +0100

    Multiple formatting changes

--- src/i4oled.c
+++ src/i4oled.c
@@ -39,24 +39,24 @@
 #define MAX_LEN 9
 
 struct params_s {
-	char* device_filename;
-	char* image_filename;
-	char* output_filename;
-	unsigned char* image;
+	char *device_filename;
+	char *image_filename;
+	char *output_filename;
+	unsigned char *image;
 	int scramble_image;
 	wchar_t text[SIZE+1];
 };
 
-void i4oled_split_text(wchar_t *source, char* line1, char* line2)
+void i4oled_split_text(wchar_t *source, char *line1, char *line2)
 {
 	wchar_t buf[SIZE+1] = L"";
 	wchar_t delimiters[SIZE+1] = L" -+_";
 	wchar_t wcsline1[SIZE+1] = L"";
 	wchar_t wcsline2[SIZE+1] = L"";
-	wchar_t* token;
-	wchar_t* state;
+	wchar_t *token;
+	wchar_t *state;
 	int i;
-	int token_len[SIZE >> 1]; /*Maximum number of tokens equals half of maximum number of characters*/
+	int token_len[SIZE >> 1]; /*Max number of tokens is half of max number of characters*/
 	size_t length, l;
 
 	wcscpy(buf, source);
@@ -99,32 +99,32 @@ out:
 
 	length = wcslen(wcsline2) + 1;
 	if (length > 1) {
-		l = wcstombs(line2, wcsline2, length );
+		l = wcstombs(line2, wcsline2, length);
 		if (l == -1)
 			wprintf(L"Invalid character sequance - please try a different text\n");
 	}
 }
 
-void i4oled_text_to_image(struct params_s* params, cairo_surface_t *surface)
+void i4oled_text_to_image(struct params_s *params, cairo_surface_t *surface)
 {
-	unsigned char* csurf;
+	unsigned char *csurf;
 	int i, x, y;
 	unsigned char lo, hi;
 
 	cairo_surface_flush(surface);
 	csurf = cairo_image_surface_get_data(surface);
 	i = 0;
-        for (y = 0; y < 32; y++) {
-                for (x = 0; x < (64 >> 1); x++) {
+	for (y = 0; y < 32; y++) {
+		for (x = 0; x < (64 >> 1); x++) {
 			hi = 0xf0 & csurf[256 * y + 8 * x + 1];
 			lo = 0x0f & (csurf[256 * y + 8 * x + 5] >> 4);
 			params->image[i] = hi | lo;
 			i++;
-                }
-        }
+		}
+	}
 }
 
-int i4oled_render_text(struct params_s* params) 
+int i4oled_render_text(struct params_s *params)
 {
 	cairo_t *cr;
 	cairo_status_t status;
@@ -137,7 +137,7 @@ int i4oled_render_text(struct params_s* params)
 	char line2[SIZE+1] = "";
 	char buf[SIZE+1];
 
-	i4oled_split_text(params->text ,line1, line2);
+	i4oled_split_text(params->text, line1, line2);
 	strcpy(buf, line1);
 	strcat(buf, "\n");
 	strcat(buf, line2);
@@ -193,7 +193,7 @@ int i4oled_render_text(struct params_s* params)
 	return 0;
 }
 
-static int i4oled_read_image(struct params_s* params)
+static int i4oled_read_image(struct params_s *params)
 {
 	unsigned char header[8];
 	unsigned char lo, hi;
@@ -204,93 +204,94 @@ static int i4oled_read_image(struct params_s* params)
 	png_byte bit_depth;
 	png_structp png_ptr;
 	png_infop info_ptr;
-	png_bytep * row_pointers;
+	png_bytep *row_pointers;
 
 	FILE *fd = fopen(params->image_filename, "r");
 	if (!fd) {
 		ret = 1;
-		wprintf(L"Failed to open params->filename: %s\n", params->image_filename);
+		wprintf(L"Failed to open params->filename: %s\n",
+				params->image_filename);
 		goto out;
 	}
 
-        fread(header, 1, 8, fd);
-        if (png_sig_cmp(header, 0, 8)) {
+	fread(header, 1, 8, fd);
+	if (png_sig_cmp(header, 0, 8)) {
 		ret = 1;
-                wprintf(L"File %s is not a PNG file", params->image_filename);
+		wprintf(L"File %s is not a PNG file", params->image_filename);
 		goto out;
 	}
 
-        png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 
-        if (!png_ptr) {
+	if (!png_ptr) {
 		ret = 1;
-                wprintf(L"png_create_read_struct failed");
+		wprintf(L"png_create_read_struct failed");
 		goto out;
 	}
 
-        info_ptr = png_create_info_struct(png_ptr);
-        if (!info_ptr) {
+	info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr) {
 		ret = 1;
-                wprintf(L"png_create_info_struct failed");
+		wprintf(L"png_create_info_struct failed");
 		goto out;
 	}
 
-        if (setjmp(png_jmpbuf(png_ptr))) {
+	if (setjmp(png_jmpbuf(png_ptr))) {
 		ret = 1;
-                wprintf(L"Error during init_io");
+		wprintf(L"Error during init_io");
 		goto out;
 	}
 
-        png_init_io(png_ptr, fd);
-        png_set_sig_bytes(png_ptr, 8);
+	png_init_io(png_ptr, fd);
+	png_set_sig_bytes(png_ptr, 8);
 
-        png_read_info(png_ptr, info_ptr);
+	png_read_info(png_ptr, info_ptr);
 
-        width = png_get_image_width(png_ptr, info_ptr);
-        height = png_get_image_height(png_ptr, info_ptr);
-        color_type = png_get_color_type(png_ptr, info_ptr);
-        bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+	width = png_get_image_width(png_ptr, info_ptr);
+	height = png_get_image_height(png_ptr, info_ptr);
+	color_type = png_get_color_type(png_ptr, info_ptr);
+	bit_depth = png_get_bit_depth(png_ptr, info_ptr);
 
-	if (width != 64 || height !=32) {
+	if (width != 64 || height != 32) {
 		ret = 1;
-                wprintf(L"Invalid image size: %dx%d, but expecting 64x32\n", width, height);
+		wprintf(L"Invalid image size: %dx%d, but expecting 64x32\n", width, height);
 		goto out;
 	}
 
-	if (color_type != 6 || bit_depth !=8) {
+	if (color_type != 6 || bit_depth != 8) {
 		ret = 1;
-                wprintf(L"Invalid color type or bit depth, please use RGBA 8-bit png\n"
+		wprintf(L"Invalid color type or bit depth, please use RGBA 8-bit png\n"
 			"Use 'file' command on the icon. Expected result:\n"
 			"PNG image data, 64 x 32, 8-bit/color RGBA, non-interlaced\n");
 		goto out;
 	}
 
-        png_read_update_info(png_ptr, info_ptr);
+	png_read_update_info(png_ptr, info_ptr);
 
-        if (setjmp(png_jmpbuf(png_ptr))) {
+	if (setjmp(png_jmpbuf(png_ptr))) {
 		ret = 1;
-                wprintf(L"Error reading image");
+		wprintf(L"Error reading image");
 		goto out;
 	}
 
-        row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
-        for (y = 0; y < height; y++)
-                row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
+	row_pointers = (png_bytep *) malloc(sizeof(png_bytep) * height);
+	for (y = 0; y < height; y++)
+		row_pointers[y] = (png_byte *) malloc(png_get_rowbytes(png_ptr, info_ptr));
 
-        png_read_image(png_ptr, row_pointers);
+	png_read_image(png_ptr, row_pointers);
 	png_set_strip_16(png_ptr);
 	png_set_packing(png_ptr);
 
 	i = 0;
-        for (y = 0; y < height; y++) {
-                png_byte* row = row_pointers[y];
-                for (x = 0; x < (width >> 1) ; x++) {
-                        png_byte* ptr = &(row[x * 8]);
+	for (y = 0; y < height; y++) {
+		png_byte *row = row_pointers[y];
+		for (x = 0; x < (width >> 1) ; x++) {
+			png_byte *ptr = &(row[x * 8]);
 			hi = 0xf0 & ptr[0];
 			lo = 0x0f & (ptr[4] >> 4);
 			params->image[i++] = hi | lo;
-                }
-        }
+		}
+	}
 
 out:
 	if (fd)
@@ -298,21 +299,21 @@ out:
 	return ret;
 }
 
-static int i4oled_oled_write(struct params_s* params)
+static int i4oled_oled_write(struct params_s *params)
 {
 	int retval;
 	int length = 1024;
 	int fd = -1;
 	int ret = 0;
 
-	fd = open (params->device_filename , O_WRONLY);
+	fd = open(params->device_filename, O_WRONLY);
 	if (fd < 0) {
 		ret = 1;
 		wprintf(L"Failed to open filename: %s\n", params->device_filename);
 		goto out;
 	}
 
-	retval = write (fd, params->image, length);
+	retval = write(fd, params->image, length);
 	if (retval != length) {
 		ret = 1;
 		wprintf(L"Writing to %s failed\n", params->device_filename);
@@ -320,30 +321,30 @@ static int i4oled_oled_write(struct params_s* params)
 	}
 out:
 	if (fd >= 0)
- 		close (fd);
+		close(fd);
 	return ret;
 }
 
-static void i4oled_scramble(struct params_s* params)
+static void i4oled_scramble(struct params_s *params)
 {
-        unsigned char buf[1024];
-        int x, y, i;
-	unsigned char l1,l2,h1,h2;
+	unsigned char buf[1024];
+	int x, y, i;
+	unsigned char l1, l2, h1, h2;
 
-        for (i = 0; i < 1024; i++)
-                buf[i] = params->image[i];
+	for (i = 0; i < 1024; i++)
+		buf[i] = params->image[i];
 
-        for (y = 0; y < 16; y++) {
-                for (x = 0; x < 32; x++) {
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 32; x++) {
 			l1 = (0x0F & (buf[31 - x + 64 * y]));
 			l2 = (0x0F & (buf[31 - x + 64 * y] >> 4));
 			h1 = (0xF0 & (buf[63 - x + 64 * y] << 4));
 			h2 = (0xF0 & (buf[63 - x + 64 * y]));
 
-                        params->image[(2 * x) + (64 * y)] = h1 | l1;
-                        params->image[(2 * x) + 1 + (64 * y)] = h2 | l2;
-                }
-        }
+			params->image[(2 * x) + (64 * y)] = h1 | l1;
+			params->image[(2 * x) + 1 + (64 * y)] = h2 | l2;
+		}
+	}
 }
 
 static void i4oled_version(void)
@@ -358,12 +359,12 @@ static void i4oled_usage(void)
 	L"i4oled sets OLED icon on Intuos4 tablets. Also converts text to png image ready for use as Intuos4 OLED icon.\n"
 	L"Usage: i4oled [options] [device image]\n"
 	L"Options:\n"
-	L" -h, --help                 - usage\n"
-	L" -d, --device               - path to OLED sysfs entry\n"
-	L" -o, --output         	     - output png file\n"
-	L" -s, --scramble             - scramble image before sending. Useful for kernel without the 'scramble' patch\n"
-	L" -t, --text         	     - text string for convertsion into image\n"
-	L" -V, --version              - version info\n");
+	L" -h, --help		- usage\n"
+	L" -d, --device		- path to OLED sysfs entry\n"
+	L" -o, --output		- output png file\n"
+	L" -s, --scramble	- scramble image before sending. Useful for kernel without the 'scramble' patch\n"
+	L" -t, --text		- text string for convertsion into image\n"
+	L" -V, --version	- version info\n");
 
 	wprintf(
 	L"Usage:\n"
@@ -372,10 +373,10 @@ static void i4oled_usage(void)
 	L"Make sure OLED brightness is set, otherwise icons will be black\n"
 	L"echo 200 > /sys/bus/usb/drivers/wacom/3-1.2:1.0/wacom_led/buttons_luminance\n"
 	L"Expected image format is:\n"
-	L"PNG image file, has to be 64 x 32, 8-bit/color RGBA, non-interlaced \n");
+	L"PNG image file, has to be 64 x 32, 8-bit/color RGBA, non-interlaced\n");
 }
 
-int i4oled_acquire_text(struct params_s* params, char* char_text)
+int i4oled_acquire_text(struct params_s *params, char *char_text)
 {
 	int l, length;
 	l = strlen(char_text);
@@ -394,7 +395,7 @@ int i4oled_acquire_text(struct params_s* params, char* char_text)
 	return 0;
 }
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	int c, ret = 0;
 	int input_present = 0;
@@ -419,7 +420,7 @@ int main (int argc, char **argv)
 		return 1;
 	}
 
-	params.device_filename= NULL;
+	params.device_filename = NULL;
 	params.image_filename = NULL;
 	params.output_filename = NULL;
 	params.image = malloc(1024);
@@ -435,7 +436,7 @@ int main (int argc, char **argv)
 	while ((c = getopt_long(argc, argv, "hd:i:o:st:V", options, &optidx)) != -1) {
 		switch (c) {
 		case 0:
-			switch(optidx){
+			switch (optidx) {
 			case 0:
 				i4oled_usage();
 				ret = 0;
@@ -486,7 +487,7 @@ int main (int argc, char **argv)
 		case 't':
 			if (i4oled_acquire_text(&params, argv[optind-1])) {
 				ret = 1;
-				goto out;	
+				goto out;
 			}
 			input_present++;
 			break;
@@ -540,9 +541,8 @@ int main (int argc, char **argv)
 	if (params.scramble_image)
 		i4oled_scramble(&params);
 
-	if (params.device_filename){
+	if (params.device_filename)
 		ret = i4oled_oled_write(&params);
-	}
 
 out:
 	free(params.image);
