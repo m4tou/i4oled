b7c2a1b199083c9fd0e81dcc4ac6b9bd988ba9e2
Przemo Firszt <przemo@firszt.eu>
2013-05-14 22:31:50 +0100

    Add 2013 to copyright date

--- src/i4oled.c
+++ src/i4oled.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2012 Przemo Firszt
+ * Copyright © 2012-2013 Przemo Firszt
  *
  * Permission to use, copy, modify, distribute, and sell this software
  * and its documentation for any purpose is hereby granted without
